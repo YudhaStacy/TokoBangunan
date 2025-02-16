dbd81b25e81b1eac556e2e612c8fdf486052ebd1
YudhaStacy <ketutyuda1322@gmail.com>
2025-02-16 16:58:01 +0800

    login register work
    using mysql

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,25 +14,32 @@
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
             <img class="mx-auto h-10 w-auto"
                 src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
-            <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900 font-sans">Login ke sistem
-                xyz
+            <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900 font-sans">Masuk ke sistem
+                Toko Bangunan
             </h2>
+
         </div>
 
+
+
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form class="space-y-6" action="#" method="POST">
+            @if (session('gagal'))
+                <p class="text-red-600 text-center">{{ session('gagal') }}</p>
+            @endif
+            <form class="space-y-6" action="{{ route('login.submit') }}" method="POST">
+                @csrf
                 <div>
+                    <label for="email" class="block text-sm/6 font-medium text-gray-900">Email</label>
                     <div class="mt-2">
                         <input type="email" name="email" id="email" autocomplete="email" required
-                            placeholder="Email"
                             class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
                     </div>
                 </div>
 
                 <div>
+                    <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
                     <div class="mt-2 flex items-center">
                         <input type="password" name="password" id="password" autocomplete="current-password" required
-                            placeholder="Password"
                             class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
                         <button type="button" id="togglePassword"
                             class="ml-2 rounded-md bg-gray-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 hover:bg-gray-500 transition">
@@ -61,7 +68,7 @@
 
             <p class="mt-10 text-center text-sm/6 text-gray-500">
                 Belum punya akun?
-                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Daftar</a>
+                <a href="/registrasi" class="font-semibold text-indigo-600 hover:text-indigo-500">Daftar</a>
             </p>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('login');
-});
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
+// Route::get('/welcome', function () {
+//     return view('welcome');
+// });
+
+Route::get('/welcome', [AuthController::class, 'tampilWelcome'])->name('welcome.tampil');
+
+Route::get('/registrasi', [AuthController::class, 'tampilRegistrasi'])->name('registrasi.tampil');
+Route::post('/registrasi/submit', [AuthController::class, 'submitRegistrasi'])->name('registrasi.submit');
+
+Route::get('/login', [AuthController::class, 'tampilLogin'])->name('login.tampil');
+Route::post('/login/submit', [AuthController::class, 'submitLogin'])->name('login.submit');
