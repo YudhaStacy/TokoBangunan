3aa3f61397f23b9ecbabad17b4a6d7939cf4df1d
YudhaStacy <ketutyuda1322@gmail.com>
2025-02-18 13:04:48 +0800

    remember me

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,9 +31,19 @@ class AuthController extends Controller
     }
 
     function submitLogin(Request $request){
-        $data = $request->only('email', 'password');
+        // $data = $request->only('email', 'password');
+        $data = $request->all();
 
-        if(Auth::attempt($data)) {
+        if(Auth::attempt(['email'=>$data['email'], 'password'=>$data['password']])) {
+            
+            if(isset($data['tetapLogin'])&&!empty($data['tetapLogin'])){
+                setcookie("email",$data['email'],time()+3600);
+                setcookie("password",$data['password'],time()+3600);
+            }else{
+                setcookie("email","");
+                setcookie("password","");
+            }
+            
             $request->session()->regenerate();
             return redirect()->route('dashboard.tampil');
         } else {
