9d42883b7eb16b9dcdb45934aa987cafe41624ff
YudhaStacy <ketutyuda1322@gmail.com>
2025-02-19 19:21:58 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,16 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
-// Route::get('/welcome', [AuthController::class, 'tampilWelcome'])->name('welcome.tampil');
 
+Route::middleware('guest')->group(function(){
+  Route::get('/registrasi', [AuthController::class, 'tampilRegistrasi'])->name('registrasi.tampil');
+  Route::post('/registrasi/submit', [AuthController::class, 'submitRegistrasi'])->name('registrasi.submit');
+  Route::get('/login', [AuthController::class, 'tampilLogin'])->name('login');
+  Route::post('/login/submit', [AuthController::class, 'submitLogin'])->name('login.submit');
+});
 
-Route::get('/registrasi', [AuthController::class, 'tampilRegistrasi'])->name('registrasi.tampil');
-Route::post('/registrasi/submit', [AuthController::class, 'submitRegistrasi'])->name('registrasi.submit');
-
-Route::get('/login', [AuthController::class, 'tampilLogin'])->name('login');
-Route::post('/login/submit', [AuthController::class, 'submitLogin'])->name('login.submit');
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::get('/dashboard', [AuthController::class, 'tampilDashboard'])->name('dashboard.tampil');
 
 Route::middleware('auth')->group(function(){
   Route::get('/dashboard', [AuthController::class, 'tampilDashboard'])->name('dashboard.tampil');  
+  Route::post('/logout', [AuthController::class, 'logout'])->name('logout');  
 });
